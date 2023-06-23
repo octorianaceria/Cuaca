f9b6823d2054787a0886d5ed8e22b51e4d388dbf
feniaster <127930728+octorianaceria@users.noreply.github.com>
2023-06-23 14:51:40 +0700

    a

---
+++ app/src/main/java/com/octoriana201103650/cuaca/RootModel.java
@@ -0,0 +1,22 @@
+package com.octoriana201103650.cuaca;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class RootModel {
+    @SerializedName("list")
+    private List<ListModel> listModelList;
+
+    public RootModel() {
+
+    }
+
+    public List<ListModel> getListModelList() {
+        return listModelList;
+    }
+
+    public void setListModelList(List<ListModel> listModelList) {
+        this.listModelList = listModelList;
+    }
+}
